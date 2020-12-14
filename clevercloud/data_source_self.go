a72cf5638e4a8d0583f1edc06eae11ac03a65fbc
Gaël Reyrol <me@gaelreyrol.dev>
2020-12-14 14:35:48 +0100

    feat: add more fields to self data source

--- clevercloud/data_source_self.go
+++ clevercloud/data_source_self.go
@@ -27,8 +27,22 @@ func dataSourceSelfRead(ctx context.Context, d *schema.ResourceData, m interface
 
 	d.SetId(self.ID)
 
-	d.Set("name", self.Name)
 	d.Set("email", self.Email)
+	d.Set("name", self.Name)
+	d.Set("phone", self.Phone)
+	d.Set("address", self.Address)
+	d.Set("city", self.City)
+	d.Set("zip_code", self.ZipCode)
+	d.Set("country", self.Country)
+	d.Set("avatar", self.Avatar)
+	d.Set("creation_date", self.CreationDate)
+	d.Set("language", self.Language)
+	d.Set("email_validated", self.EmailValidated)
+	d.Set("oauth_apps", self.OAuthApps)
+	d.Set("admin", self.Admin)
+	d.Set("can_pay", self.CanPay)
+	d.Set("preferred_mfa", self.PreferredMFA)
+	d.Set("has_password", self.HasPassword)
 
 	return diags
 }
@@ -45,65 +59,65 @@ func dataSourceSelf() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			// "phone": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "address": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "city": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "zip_code": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "country": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "avatar": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "creation_date": {
-			// 	Type:     schema.TypeInt,
-			// 	Computed: true,
-			// },
-			// "langugage": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "email_validated": {
-			// 	Type:     schema.TypeBool,
-			// 	Computed: true,
-			// },
-			// "oauth_apps": {
-			// 	Type:     schema.TypeList,
-			// 	Computed: true,
-			// 	Elem: &schema.Schema{
-			// 		Type: schema.TypeString,
-			// 	},
-			// },
-			// "admin": {
-			// 	Type:     schema.TypeBool,
-			// 	Computed: true,
-			// },
-			// "can_pay": {
-			// 	Type:     schema.TypeBool,
-			// 	Computed: true,
-			// },
-			// "preferred_mfa": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "has_password": {
-			// 	Type:     schema.TypeBool,
-			// 	Computed: true,
-			// },
+			"phone": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"city": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"zip_code": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"country": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"avatar": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"creation_date": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"language": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"email_validated": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"oauth_apps": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"admin": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"can_pay": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"preferred_mfa": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"has_password": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 		},
 	}
 }
