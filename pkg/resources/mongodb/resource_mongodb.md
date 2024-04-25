77790b046df0ef876779a5bec54d2705c1df46ed
LeCrabe <mathieu.barcelo@clever-cloud.com>
2024-04-25 17:58:33 +0200

    feat: Mongo

---
+++ pkg/resources/mongodb/resource_mongodb.md
@@ -0,0 +1,3 @@
+Manage [MongoDB](https://www.mongodb.com/) product.
+
+See [product specification](https://www.clever-cloud.com/fr/product/mongodb/).
--- pkg/tmp/addon.go
+++ pkg/tmp/addon.go
@@ -100,6 +100,18 @@ func GetMateriaKV(ctx context.Context, cc *client.Client, organisationID, postgr
 	return client.Get[MateriaKV](ctx, cc, path)
 }
 
+type MongoDB struct {
+	Host   string `json:"host"`
+	Port   int64  `json:"port"`
+	Token  string `json:"token"`
+	Status string `json:"status" example:"ACTIVE"`
+}
+
+func GetMongoDB(ctx context.Context, cc *client.Client, mongodbID string) client.Response[MongoDB] {
+	path := fmt.Sprintf("/v4/addon-providers/mongodb-addon/addons/%s", mongodbID)
+	return client.Get[MongoDB](ctx, cc, path)
+}
+
 type DeleteAddonResponse struct {
 	ID      int64  `json:"id"`
 	Message string `json:"message"`
