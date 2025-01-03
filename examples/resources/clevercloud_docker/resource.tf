8b8e7fa1cba71045093ce31c5501df3913d0de50
Julien WITTOUCK <julien.wittouck@gmail.com>
2025-01-03 12:24:42 +0100

    docs(docker): add usage example

---
+++ examples/resources/clevercloud_docker/resource.tf
@@ -0,0 +1,14 @@
+resource "clevercloud_docker" "docker_instance" {
+  name = "docker_instance"
+  region = "par"
+
+  # horizontal scaling
+  min_instance_count = 1
+  max_instance_count = 2
+
+  # vertical scaling
+  smallest_flavor = "XS"
+  biggest_flavor  = "M"
+
+  additional_vhosts = ["example.com"]
+}
