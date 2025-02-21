abdd890bce173f5f8f900b34a24458e0187cf4cf
LeCrabe <mathieu.barcelo@clever-cloud.com>
2025-02-21 16:48:08 +0100

    fix(plan): remove test plan

--- pkg/resources/keycloak/schema.go
+++ pkg/resources/keycloak/schema.go
@@ -6,16 +6,17 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"go.clever-cloud.com/terraform-provider/pkg/attributes"
 )
 
 type Keycloak struct {
-	ID           types.String `tfsdk:"id"`
-	Name         types.String `tfsdk:"name"`
-	CreationDate types.Int64  `tfsdk:"creation_date"`
-	Region       types.String `tfsdk:"region"`
-	Host         types.String `tfsdk:"host"`
+	attributes.Addon
+	Host types.String `tfsdk:"host"`
+	// ID           types.String `tfsdk:"id"`
+	// Name         types.String `tfsdk:"name"`
+	// CreationDate types.Int64  `tfsdk:"creation_date"`
+	// Region       types.String `tfsdk:"region"`
 }
 
 //go:embed doc.md
@@ -25,18 +26,18 @@ func (r ResourceKeycloak) Schema(_ context.Context, req resource.SchemaRequest, 
 	resp.Schema = schema.Schema{
 		Version:             0,
 		MarkdownDescription: resourceKeycloakDoc,
-		Attributes: map[string]schema.Attribute{
-			"name": schema.StringAttribute{Required: true, MarkdownDescription: "Name of the service"},
-			"region": schema.StringAttribute{
-				Optional:            true,
-				Computed:            true,
-				Default:             stringdefault.StaticString("par"),
-				MarkdownDescription: "Geographical region where the data will be stored",
-			},
-			"id":            schema.StringAttribute{Computed: true, MarkdownDescription: "Generated unique identifier"},
-			"creation_date": schema.Int64Attribute{Computed: true, MarkdownDescription: "Date of database creation"},
-			"host":          schema.StringAttribute{Computed: true, MarkdownDescription: "URL to access Keycloak"},
-		},
+		Attributes: attributes.WithAddonCommons(map[string]schema.Attribute{
+			"host": schema.StringAttribute{Computed: true, MarkdownDescription: "URL to access Keycloak"},
+			// "name": schema.StringAttribute{Required: true, MarkdownDescription: "Name of the service"},
+			// "region": schema.StringAttribute{
+			// 	Optional:            true,
+			// 	Computed:            true,
+			// 	Default:             stringdefault.StaticString("par"),
+			// 	MarkdownDescription: "Geographical region where the data will be stored",
+			// },
+			// "id":            schema.StringAttribute{Computed: true, MarkdownDescription: "Generated unique identifier"},
+			// "creation_date": schema.Int64Attribute{Computed: true, MarkdownDescription: "Date of database creation"},
+		}),
 	}
 }
 
