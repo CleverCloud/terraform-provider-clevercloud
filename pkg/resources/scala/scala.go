431badcfedf5e7160577b81d88df9b1a53904847
miton18 <remi@collignon-ducret.fr>
2025-08-25 21:57:36 +0200

    chore: configure

--- pkg/resources/scala/scala.go
+++ pkg/resources/scala/scala.go
@@ -3,16 +3,12 @@ package scala
 import (
 	"context"
 
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
-
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"go.clever-cloud.dev/client"
+	"go.clever-cloud.com/terraform-provider/pkg/helper"
 )
 
 type ResourceScala struct {
-	cc      *client.Client
-	org     string
-	gitAuth *http.BasicAuth
+	helper.Configurer
 }
 
 func NewResourceScala() func() resource.Resource {
