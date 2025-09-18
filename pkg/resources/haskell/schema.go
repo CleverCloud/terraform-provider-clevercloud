b5191dba8a31b1882e68bdc35fb63746d5078671
Tristan Charbonneau <tristan.charbonneau@clever-cloud.com>
2025-09-18 11:24:53 +0200

    feat(runtime): add haskell runtime

---
+++ pkg/resources/haskell/schema.go
@@ -0,0 +1,66 @@
+package haskell
+
+import (
+	"context"
+	_ "embed"
+
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"go.clever-cloud.com/terraform-provider/pkg"
+	"go.clever-cloud.com/terraform-provider/pkg/application"
+	"go.clever-cloud.com/terraform-provider/pkg/attributes"
+)
+
+type Haskell struct {
+	attributes.Runtime
+}
+
+//go:embed doc.md
+var haskellDoc string
+
+func (r ResourceHaskell) Schema(ctx context.Context, req resource.SchemaRequest, res *resource.SchemaResponse) {
+
+	res.Schema = schema.Schema{
+		Version:             0,
+		MarkdownDescription: haskellDoc,
+		Attributes: attributes.WithRuntimeCommons(map[string]schema.Attribute{}),
+		Blocks: attributes.WithBlockRuntimeCommons(map[string]schema.Block{}),
+	}
+}
+
+// https://developer.hashicorp.com/terraform/plugin/framework/resources/state-upgrade#implementing-state-upgrade-support
+func (r ResourceHaskell) UpgradeState(ctx context.Context) map[int64]resource.StateUpgrader {
+	return map[int64]resource.StateUpgrader{}
+}
+
+func (haskellapp Haskell) toEnv(ctx context.Context, diags diag.Diagnostics) map[string]string {
+	env := map[string]string{}
+
+	// do not use the real map since ElementAs can nullish it
+	// https://github.com/hashicorp/terraform-plugin-framework/issues/698
+	customEnv := map[string]string{}
+	diags.Append(haskellapp.Environment.ElementsAs(ctx, &customEnv, false)...)
+	if diags.HasError() {
+		return env
+	}
+	env = pkg.Merge(env, customEnv)
+
+	pkg.IfIsSetStr(haskellapp.AppFolder, func(s string) { env["APP_FOLDER"] = s })
+	env = pkg.Merge(env, haskellapp.Hooks.ToEnv())
+
+	return env
+}
+
+func (haskellapp Haskell) toDeployment(gitAuth *http.BasicAuth) *application.Deployment {
+	if haskellapp.Deployment == nil || haskellapp.Deployment.Repository.IsNull() {
+		return nil
+	}
+
+	return &application.Deployment{
+		Repository:    haskellapp.Deployment.Repository.ValueString(),
+		Commit:        haskellapp.Deployment.Commit.ValueStringPointer(),
+		CleverGitAuth: gitAuth,
+	}
+}
