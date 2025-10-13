eff72a02b628cda44235183f914e8d562a87c55a
miton18 <remi@collignon-ducret.fr>
2025-10-13 10:25:24 +0200

    fix(python): missing upgrade state
    Fix #265
    
    Update pkg/resources/python/python.go
    
    Co-authored-by: LeCrabe <barcelo.mathieu@gmail.com>

--- pkg/resources/python/python.go
+++ pkg/resources/python/python.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"go.clever-cloud.com/terraform-provider/pkg/attributes"
 	"go.clever-cloud.com/terraform-provider/pkg/helper"
 )
 
@@ -17,4 +19,50 @@ func NewResourcePython() resource.Resource {
 
 func (r *ResourcePython) Metadata(ctx context.Context, req resource.MetadataRequest, res *resource.MetadataResponse) {
 	res.TypeName = req.ProviderTypeName + "_python"
+}
+
+// UpgradeState implements state migration from version 0 to 1 for vhosts attribute
+func (r *ResourcePython) UpgradeState(ctx context.Context) map[int64]resource.StateUpgrader {
+	return map[int64]resource.StateUpgrader{
+		0: {
+			PriorSchema: &schemaPythonV0,
+			StateUpgrader: func(ctx context.Context, req resource.UpgradeStateRequest, res *resource.UpgradeStateResponse) {
+				tflog.Info(ctx, "Upgrading Python resource state from version 0 to 1")
+
+				old := helper.StateFrom[PythonV0](ctx, *req.State, &res.Diagnostics)
+				if res.Diagnostics.HasError() {
+					return
+				}
+
+				oldVhosts := []string{}
+				res.Diagnostics.Append(old.VHosts.ElementsAs(ctx, &oldVhosts, false)...)
+				vhosts := helper.VHostsFromAPIHosts(ctx, oldVhosts, old.VHosts, &res.Diagnostics)
+
+				newState := Python{
+					Runtime: attributes.Runtime{
+						ID:               old.ID,
+						Name:             old.Name,
+						Description:      old.Description,
+						MinInstanceCount: old.MinInstanceCount,
+						MaxInstanceCount: old.MaxInstanceCount,
+						SmallestFlavor:   old.SmallestFlavor,
+						BiggestFlavor:    old.BiggestFlavor,
+						BuildFlavor:      old.BuildFlavor,
+						Region:           old.Region,
+						StickySessions:   old.StickySessions,
+						RedirectHTTPS:    old.RedirectHTTPS,
+						VHosts:           vhosts,
+						DeployURL:        old.DeployURL,
+						Dependencies:     old.Dependencies,
+						Deployment:       old.Deployment,
+						Hooks:            old.Hooks,
+						AppFolder:        old.AppFolder,
+						Environment:      old.Environment,
+					},
+				}
+
+				res.Diagnostics.Append(res.State.Set(ctx, newState)...)
+			},
+		},
+	}
 }
